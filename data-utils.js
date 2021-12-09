88c0554970f7fcea79a60c3deed588ca9e88dc0d
Victor Caruso <vic.caruso@gmail.com>
2021-12-08 18:00:19 -0800

    set up findFriendByName

--- data-utils.js
+++ data-utils.js
@@ -1,5 +1,9 @@
 export default function findFriendByName(name, friends) {
     // iterate through the friends
-    
     // when you find a friend with the correct name, return that friend
+    for (let item of friends) {
+        if (item.name === name) {
+            return item;
+        }
+    }
 }
